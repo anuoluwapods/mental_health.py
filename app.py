0ebd452c30431b4a945a638a96cd34827b6dc697
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2023-01-20 15:25:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from hasher import Hasher
 import streamlit_authenticator as stauth
 from authenticate import Authenticate
 
+
+tab1, tab2, tab3 = st.tabs(["Mood Check", "Maniac & Depression Signs", "Wellness Signs"])
 hashed_passwords = stauth.Hasher(['Creativeart1.']).generate()
 with open(r'config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
@@ -27,7 +29,71 @@ if  authentication_status:
         authenticator.logout('Logout', 'main')
 if username == 'Anuoluwapo Balogun':
         st.write(f'Welcome *{name}*')
-        tab1, tab2, tab3 = st.tabs(["Mood Check", "Maniac & Depression Signs", "Wellness Signs"])
+        with tab1:
+         col1, col2 = st.columns(2)
+         col3, col4 = st.columns(2)
+         col1.header("My Mental Health App")
+         col1.write("tracking my mental health so i can predict my mood with enough data accurately and become better")
+
+    #col2.image(image)
+
+    deta =  Deta(st.secrets["deta_key"])
+
+
+    db = deta.Base("mood-check") 
+
+    mood = st.radio("How Do You Feel Today",
+          ('Choose', 'Happy', 'Sad', 'Let me explain'))
+    if mood == 'Happy':
+       db.put({"Mood":mood})
+
+    if mood == 'Sad':
+       db.put({"Mood":mood})
+
+    if mood == 'Let me explain':
+       st.textinput("Explain Your Mood")
+       db.put({"Mood":mood})
+
+    with tab2:
+       col1, col2 = st.columns(2)
+       col3, col4 = st.columns(2)
+
+    col1.header("My Mental Health App")
+    col1.write("tracking my mental health so i can predict my mood with enough data accurately and become better")
+
+    deta1 = Deta(st.secrets["deta_key1"])
+    db1 = deta1.Base("maniac-depression-signs")
+
+    with st.form("form", clear_on_submit=True):
+        maniac = st.text_input("Explain Your Mood ")
+        submitted = st.form_submit_button("Submit")
+    if submitted:
+          db.put({"Maniac Signs": maniac})
+
+
+    with tab3:
+      col1, col2 = st.columns(2)
+      col3, col4 = st.columns(2)
+      col1.header("My Mental Health App")
+      col1.write("tracking my mental health so i can predict my mood with enough data accurately and become better")
+
+    #col2.image(image)
+
+    deta3 =  Deta(st.secrets["deta_key3"])
+
+
+    db3 = deta3.Base("mood-check") 
+
+    healthy = st.radio("How Do You Feel Today",
+        ('Choose','Healthy', 'Sick'))
+    if healthy == 'Healthy':
+      db.put({"Healthy":healthy})
+
+    if healthy == 'Sick':
+      db.put({"Healthy":healthy})
+
+
+        
 elif authentication_status == False:
         st.error('Username/password is incorrect')
 elif authentication_status == None:
@@ -36,69 +102,6 @@ elif authentication_status == None:
 
 
 
-
-with tab1:
-     col1, col2 = st.columns(2)
-     col3, col4 = st.columns(2)
-     col1.header("My Mental Health App")
-     col1.write("tracking my mental health so i can predict my mood with enough data accurately and become better")
-
-#col2.image(image)
-
-deta =  Deta(st.secrets["deta_key"])
- 
-
-db = deta.Base("mood-check") 
-
-mood = st.radio("How Do You Feel Today",
-      ('Choose', 'Happy', 'Sad', 'Let me explain'))
-if mood == 'Happy':
-   db.put({"Mood":mood})
-  
-if mood == 'Sad':
-   db.put({"Mood":mood})
-
-if mood == 'Let me explain':
-   st.textinput("Explain Your Mood")
-   db.put({"Mood":mood})
-
-with tab2:
-   col1, col2 = st.columns(2)
-   col3, col4 = st.columns(2)
-
-col1.header("My Mental Health App")
-col1.write("tracking my mental health so i can predict my mood with enough data accurately and become better")
-
-deta1 = Deta(st.secrets["deta_key1"])
-db1 = deta1.Base("maniac-depression-signs")
-
-with st.form("form", clear_on_submit=True):
-    maniac = st.text_input("Explain Your Mood ")
-    submitted = st.form_submit_button("Submit")
-if submitted:
-      db.put({"Maniac Signs": maniac})
-
-
-with tab3:
-  col1, col2 = st.columns(2)
-  col3, col4 = st.columns(2)
-  col1.header("My Mental Health App")
-  col1.write("tracking my mental health so i can predict my mood with enough data accurately and become better")
-
-#col2.image(image)
-
-deta3 =  Deta(st.secrets["deta_key3"])
-
-
-db3 = deta3.Base("mood-check") 
-
-healthy = st.radio("How Do You Feel Today",
-    ('Choose','Healthy', 'Sick'))
-if healthy == 'Healthy':
-  db.put({"Healthy":healthy})
-
-if healthy == 'Sick':
-  db.put({"Healthy":healthy})
 
 
 
