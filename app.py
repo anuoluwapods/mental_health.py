a488aa58ffeb6922a6d07b4d2a54ab1ef2a6cb7e
Anuoluwapo <83256563+anuoluwapods@users.noreply.github.com>
2023-01-20 15:06:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,14 @@ hashed_passwords = stauth.Hasher(['Creativeart1.']).generate()
 with open(r'config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
     
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+    
 name, authentication_status, username = authenticator.login('Login', 'main')
   
 tab1, tab2, tab3 = st.tabs(["Mood Check", "Maniac & Depression Signs", "Wellness Signs"])
